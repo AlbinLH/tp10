f66428bcf424c2aa3474fe7a11d2c633f386bc40
AlbinLH <32388806+AlbinLH@users.noreply.github.com>
2017-12-15 11:26:59 +0100

    Add files via upload

--- src/tp10/Dictionnaire.java
+++ src/tp10/Dictionnaire.java
@@ -4,6 +4,7 @@ import java.nio.file.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class Dictionnaire {
 	public String filename;
@@ -27,14 +28,14 @@ public class Dictionnaire {
 		}
 		return false;
 	}
-	
+
 	public String choose() {
 		String word = new String();
 		Path p = Paths.get(filename);
 		try {
 			List<String> lblContent = new ArrayList<String>();
 			lblContent = Files.readAllLines(p);
-			int number = (int)Math.random()* 0 + lblContent.size();
+			int number = (int) Math.random() * 0 + lblContent.size();
 			word = lblContent.get(number);
 			return word;
 		} catch (IOException e) {
@@ -42,5 +43,26 @@ public class Dictionnaire {
 		}
 		return null;
 	}
-	
+
+	public String evaluer(String word, String tentative) {
+		String reponse = null;
+		List<String> listword = Arrays.asList(word);
+		List<String> listtentative = Arrays.asList(tentative);
+		int taille = listword.size();
+		System.out.println("Le mot a "+ taille +" lettres et commence par "+ listword.get(0));
+		if (listword.size() == listtentative.size()) {
+			for (int i = 0; i < listword.size(); i++) {
+				if (listtentative.get(i) == listword.get(i)) {
+					reponse += "o";
+				} else if (listword.contains(listtentative.get(i))) {
+					reponse += "-";
+				} else {
+					reponse += "x";
+				}
+				listword.remove(i);
+			}
+			return (reponse);
+		}
+		return (null);
+	}
 }
